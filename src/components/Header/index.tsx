177efff9e3c60986c8e6981bd880a4d275306743
Rellyso <rellysondouglas2015@gmail.com>
2021-04-29 08:51:40 -0300

    created Header component

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,3 +1,15 @@
+import styles from './header.module.scss'
+
 export default function Header() {
-  // TODO
+  return (
+    <header className={styles.headerContainer}>
+      <div className={styles.headerContent}>
+        <span>
+          <img src="/logo.svg" alt="spacetraveling" />
+          spacetraveling
+          <strong>.</strong>
+        </span>
+      </div>
+    </header>
+  )
 }
