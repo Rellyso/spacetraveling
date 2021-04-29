557dc3b2c8b12851feb2cc15833b342a3e5418db
Rellyso <rellysondouglas2015@gmail.com>
2021-04-29 08:52:02 -0300

    getting homepage and its associated styles

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,8 @@
-import { GetStaticProps } from 'next';
+// import { GetStaticProps } from 'next';
 
-import { getPrismicClient } from '../services/prismic';
+// import { getPrismicClient } from '../services/prismic';
+
+import { FiCalendar, FiUser } from 'react-icons/fi'
 
 import commonStyles from '../styles/common.module.scss';
 import styles from './home.module.scss';
@@ -24,9 +26,51 @@ interface HomeProps {
   postsPagination: PostPagination;
 }
 
-// export default function Home() {
-//   // TODO
-// }
+export default function Home() {
+  return (
+    <>
+      <main className={styles.homeContainer}>
+        <div className={styles.homeContent}>
+          <a>
+            <h1>Como Utilizar Hooks</h1>
+            <p>Pensando em sincronização em vez de ciclos de vida.</p>
+            <div>
+              <time>
+                <FiCalendar />
+              15 Mar 2021
+            </time>
+              <span>
+                <FiUser />
+              Joseph Oliveira
+            </span>
+            </div>
+          </a>
+
+          <a>
+            <h1>Como Utilizar Hooks</h1>
+            <p>Pensando em sincronização em vez de ciclos de vida.</p>
+            <div>
+              <time>
+                <FiCalendar />
+              15 Mar 2021
+            </time>
+              <span>
+                <FiUser />
+              Joseph Oliveira
+            </span>
+            </div>
+          </a>
+
+          <button type="button">
+            Carregar mais posts
+          </button>
+        </div>
+
+
+      </main>
+    </>
+  )
+}
 
 // export const getStaticProps = async () => {
 //   // const prismic = getPrismicClient();
